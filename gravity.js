d9aa75c1bf2c66272073e5ba6bfcc3b04123b70c
Stanislav Rogovskiy <stanislav.rogovskiy@gmail.com>
2014-04-19 23:52:27 +0300

    Rewritten in OOP style

--- gravity.js
+++ gravity.js
@@ -1,166 +1,138 @@
 var canvas, context;
 var size = 700;
 var height = size, width = size;
-var center = [Math.floor(width/2), Math.floor(height/2)];
+var center = new Vector(Math.floor(width/2), Math.floor(height/2));
 
-var masses;
 var bodies;
-var radius = 5, mass = 10;
-var G = 1;
-var bodiesCount = 30;
+var mass = 5;
+var G = 100;
+var bodiesCount = 2;
 var t = 0, dt = 0.05;
+var c = 0;
 
 function init() {
 	canvas = document.getElementById('canvas');
-	context = canvas.getContext('2d');
 	canvas.width = width;
 	canvas.height = height;
 
-	context.fillStyle="white";
-	context.fillRect(0,0,width,height);
+	context = canvas.getContext('2d');
 	createBodies(bodiesCount);
 	run();
 }
 
 function createBodies(bodiesCount) {
 	bodies = new Array(bodiesCount);
-	masses = new Array(bodiesCount);
 
-	var R = size/10, alpha;
+	var R = size/4, alpha;
 	for (var i = bodiesCount-1; i>=0; --i) {
-		masses[i] = (Math.random()-0.5)*10;
-		bodies[i] = new Array(2);
-		alpha = 2*Math.PI*i/bodiesCount;
-		bodies[i][0] = [center[0] + (Math.random()-0.5)*100, center[1] + (Math.random()-0.5)*100];
-		// bodies[i][0] = [center[0] + Math.round(R*Math.sin(alpha)), center[1] - Math.round(R*Math.cos(alpha))];
-		bodies[i][1] = [0, 0];
+		bodies[i] = new Body(i, center.add(new Vector(Math.floor(R*Math.random()), Math.floor(R*Math.random()))),
+			new Vector(0, 0), mass);
 	}
-
-	console.debug(bodies);
 }
 
 function run() {
 	context.fillStyle="white";
 	context.fillRect(0,0,width,height);
-	drawBodies(bodies);
-	bodies = nextPosition(bodies, masses, dt);
+	draw(bodies, context);
+	console.log(bodies[0].pos, bodies[0].v)
+	bodies = nextPosition(bodies, dt);
 	t += dt;
 	requestAnimationFrame(run);
 }
 
-function nextPosition (bodies, masses, dt) {
-
-	var m = masses;
-	var c = new Array(4);
-	c[0] = F(bodies, masses);
-	for (var i = bodiesCount-1; i>=0; --i) {
-		for (var j = 1; j>=0; --j) {
-			for (var k = 1; k>=0; --k) {
-				c[0][i][j][k]*= dt;
-			}
-		}
-	}
-
-	var xn = bodies.concat();
-	for (var i = bodiesCount-1; i>=0; --i) {
-		for (var j = 1; j>=0; --j) {
-			for (var k = 1; k>=0; --k) {
-				xn[i][j][k] += c[0][i][j][k]/2.0;
-			}
-		}
-	}
-	c[1] = F(xn, masses);
-	for (var i = bodiesCount-1; i>=0; --i) {
-		for (var j = 1; j>=0; --j) {
-			for (var k = 1; k>=0; --k) {
-				c[1][i][j][k] *= dt;
-			}
-		}
-	}
-
-	xn = bodies.concat();
-	for (var i = bodiesCount-1; i>=0; --i) {
-		for (var j = 1; j>=0; --j) {
-			for (var k = 1; k>=0; --k) {
-				xn[i][j][k] += c[1][i][j][k]/2.0;
-			}
-		}
-	}
-	c[2] = F(xn, masses);
-	for (var i = bodiesCount-1; i>=0; --i) {
-		for (var j = 1; j>=0; --j) {
-			for (var k = 1; k>=0; --k) {
-				c[2][i][j][k]*= dt;
-			}
-		}
-	}
-
-	xn = bodies.concat();
-	for (var i = bodiesCount-1; i>=0; --i) {
-		for (var j = 1; j>=0; --j) {
-			for (var k = 1; k>=0; --k) {
-				xn[i][j][k] += c[2][i][j][k];
-			}
-		}
-	}
-	c[3] = F(xn, masses);
-	for (var i = bodiesCount-1; i>=0; --i) {
-		for (var j = 1; j>=0; --j) {
-			for (var k = 1; k>=0; --k) {
-				c[3][i][j][k]*= dt;
-			}
-		}
-	}
-
-	for (var i = bodiesCount-1; i>=0; --i) {
-		for (var j = 1; j>=0; --j) {
-			for (var k = 1; k>=0; --k) {
-				bodies[i][j][k] += (c[0][i][j][k] + 2.0*c[1][i][j][k] + 2.0*c[2][i][j][k] + c[3][i][j][k])/6.0;
-			}
-		}
-	}
-
-	return bodies;
+function nextPosition (bodies, dt) {
+	var k1 = multiply(F(bodies, G), dt);
+	var k2 = multiply(F(add(bodies, multiply(k1, 0.5)), G), dt);
+	var k3 = multiply(F(add(bodies, multiply(k2, 0.5)), G), dt);
+	var k4 = multiply(F(add(bodies, k3), G), dt);
+	return add(bodies, multiply(add(k1, add(multiply(k2, 2.0), add(multiply(k3, 2.0), k4))), 1.0/6.0));
 }
 
-
-function F(x, masses) {
-	var result = new Array(bodiesCount);
-	for (var i = bodiesCount-1; i>=0; --i) {
-		result[i] = new Array(2);
-		for (var j = 1; j>=0; --j)
-			result[i][j] = new Array(2);
-
-		for (var k = 1; k>=0; --k) {
-			result[i][0][k] = x[i][1][k];
-		}
-		result[i][1][0] = 0;
-		result[i][1][1] = 0;
-		for (var j = bodiesCount-1; j>=0; --j) 
+function F(bodies, G) {
+	var result = new Array(bodies.length);
+	for (var i = bodies.length-1; i>=0; --i) {
+		for (var j = bodies.length-1; j>=0; --j) 
 			if (i != j) {
-				var cd = cube(distance(x[i][0], x[j][0]));
-				for (var k = 1; k>=0; --k) {
-					result[i][1][k] += G*masses[j]*(x[j][0][k] - x[i][0][k])/cd; 
-				}
-			}
+			result[i] = bodies[i].F(bodies[j], G);
 		}
+	}
 	return result;
 }
 
-function drawBodies(bodies) {
-	context.fillStyle="black";
-	context.beginPath();
-	for (var i = bodiesCount-1; i>=0; --i) {
-		context.moveTo(bodies[i][0][0], bodies[i][0][1]);
-		context.arc(bodies[i][0][0], bodies[i][0][1], radius, 0, 2*Math.PI);
+function add(bodies, that) {
+	var result = new Array(bodies.length);
+	for (var i = bodies.length-1; i>=0; --i) {
+		result[i] = bodies[i].add(that[i].pos, that[i].v);
 	}
-	context.fill();
+	return result;
 }
 
-function distance(a, b) {
-	return Math.sqrt((a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]));
+function multiply(bodies, a) {
+	var result = new Array(bodies.length);
+	for (var i = bodies.length-1; i>=0; --i) {
+		result[i] = new Body(i, bodies[i].pos.multiply(a), bodies[i].v.multiply(a), bodies[i].m);
+	}
+	return result;
+}
+ 
+function draw (bodies, context) {
+	for (var i = bodies.length-1; i>=0; --i) {
+		bodies[i].draw(context);
+	}
 }
 
-function cube(a) {
-	return a*a*a;
+function Body (id, pos, v, m) {
+	this.id = id;
+	this.pos = pos;
+	this.v = v;
+	this.m = m;
+	this.radius = m;
+
+	this.draw = function (context) {
+		context.fillStyle="black";
+
+		// context.save();
+        // context.translate(this.pos.x, this.pos.y);
+        context.beginPath();
+        context.arc(this.pos.x, this.pos.y, this.radius, 0, 2*Math.PI);
+        // context.closePath();
+        context.fill();
+        // context.restore();
+	}
+
+	this.F = function (that, G) {
+		var result = new Body(id, this.v, new Vector(0, 0), m);
+		var diff = this.pos.subtract(that.pos);
+		result.v = result.v.add(diff.multiply(-G*that.m/cube(diff.norm())));
+		return result;
+
+		function cube(a) {
+			return a*a*a;
+		}
+	}
+
+	this.add = function(sp, sv) {
+		return new Body(id, this.pos.add(sp), this.v.add(sv), this.m);
+	}	
+}
+
+function Vector(x, y) {
+	this.x = x;
+	this.y = y;
+
+	this.add = function (that) {
+		return new Vector(this.x+that.x, this.y+that.y);
+	}
+
+	this.subtract = function (that) {
+		return new Vector(this.x-that.x, this.y-that.y);
+	}
+
+	this.multiply = function (a) {
+		return new Vector(this.x*a, this.y*a);
+	}
+
+	this.norm = function (){
+		return Math.sqrt(this.x*this.x + this.y*this.y);
+	}
 }
