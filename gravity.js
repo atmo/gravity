8e11fd47c75caea76183b2ba17612c52be9779ab
Stanislav Rogovskiy <stanislav.rogovskiy@gmail.com>
2014-04-24 20:10:56 +0300

    Translated tabs to spaces

--- gravity.js
+++ gravity.js
@@ -10,195 +10,198 @@ var bodiesCount = 50;
 var t = 0, dt = 0.1;
 
 function init() {
-	canvas = document.getElementById('canvas');
-	canvas.width = width;
-	canvas.height = height;
+    canvas = document.getElementById('canvas');
+    canvas.width = width;
+    canvas.height = height;
 
-	context = canvas.getContext('2d');
-	cluster = new Cluster(bodiesCount, width, height, G);
-	run();
+    context = canvas.getContext('2d');
+    cluster = new Cluster(bodiesCount, width, height, G);
+    run();
 }
 
 function run() {
-	context.fillStyle="white";
-	context.fillRect(0,0,width,height);
-	cluster.draw(context);
-	cluster = cluster.nextPosition(dt);
-	cluster.checkHit();
-	cluster.checkBounce(width, height);
-	t += dt;
-	requestAnimationFrame(run);
+    context.fillStyle="white";
+    context.fillRect(0,0,width,height);
+    cluster.draw(context);
+    cluster = cluster.nextPosition(dt);
+    cluster.checkHit();
+    cluster.checkBounce();
+    t += dt;
+    requestAnimationFrame(run);
 }
 
 function Cluster(bodies, width, height, G) {
-	this.bodies = typeof bodies == "number" ? createBodies(bodies) : bodies;
-	this.width = width;
-	this.height = height;
-	this.G = G;
+    this.bodies = typeof bodies == "number" ? createBodies(bodies) : bodies;
+    this.width = width;
+    this.height = height;
+    this.G = G;
 
-	function createBodies (bodiesCount) {
-		var b = new Array(bodiesCount);
+    function createBodies (bodiesCount) {
+        var b = new Array(bodiesCount);
 
-		var R = size/8, alpha;
-		for (var i = bodiesCount-1; i>=0; --i) {
-			b[i] = new Body(i, randomCenter(center, R),
-				new Vector(0, 0), mass);
-		}
+        var R = size/8, alpha;
+        for (var i = bodiesCount-1; i>=0; --i) {
+            b[i] = new Body(i, randomCenter(center, R),
+                new Vector(0, 0), mass);
+        }
 
-		return b;
+        return b;
 
-		function randomCenter (center, s) {
-			return center.add(new Vector(Math.floor(2*s*(Math.random()-0.5)), Math.floor(2*s*(Math.random()-0.5))));
-		}
-	}
+        function randomCenter (center, s) {
+            return center.add(new Vector(Math.floor(2*s*(Math.random()-0.5)), Math.floor(2*s*(Math.random()-0.5))));
+        }
+    }
 
-	this.F = function() {
-		var result = new Array(this.bodies.length);
-		for (var i = this.bodies.length-1; i>=0; --i) {
-			for (var j = this.bodies.length-1; j>=0; --j) 
-				if (i != j) {
-					result[i] = this.bodies[i].F(this.bodies[j], this.G);
-			}
-		}
-		return new Cluster(result, this.width, this.height, this.G);
-	}
+    this.F = function() {
+        var result = new Array(this.bodies.length);
+        for (var i = this.bodies.length-1; i>=0; --i) {
+            for (var j = this.bodies.length-1; j>=0; --j)
+                if (i != j) {
+                    result[i] = this.bodies[i].F(this.bodies[j], this.G);
+            }
+        }
+        return new Cluster(result, this.width, this.height, this.G);
+    }
 
-	this.add = function(that) {
-		var result = new Array(this.bodies.length);
-		for (var i = this.bodies.length-1; i>=0; --i) {
-			result[i] = this.bodies[i].add(that.bodies[i].pos, that.bodies[i].v);
-		}
-		return new Cluster(result, this.width, this.height, this.G);
-	}
+    this.add = function(that) {
+        var result = new Array(this.bodies.length);
+        for (var i = this.bodies.length-1; i>=0; --i) {
+            result[i] = this.bodies[i].add(that.bodies[i].pos, that.bodies[i].v);
+        }
+        return new Cluster(result, this.width, this.height, this.G);
+    }
 
-	this.multiply = function (a) {
-		var result = new Array(this.bodies.length);
-		for (var i = this.bodies.length-1; i>=0; --i) {
-			result[i] = new Body(i, this.bodies[i].pos.multiply(a), this.bodies[i].v.multiply(a), this.bodies[i].m);
-		}
-		return new Cluster(result, this.width, this.height, this.G);
-	}
-	 
-	this.draw = function(context) {
-		for (var i = this.bodies.length-1; i>=0; --i) {
-			this.bodies[i].draw(context);
-		}
-	}
+    this.multiply = function (a) {
+        var result = new Array(this.bodies.length);
+        for (var i = this.bodies.length-1; i>=0; --i) {
+            result[i] = new Body(i, this.bodies[i].pos.multiply(a), this.bodies[i].v.multiply(a), this.bodies[i].m);
+        }
+        return new Cluster(result, this.width, this.height, this.G);
+    }
 
-	this.checkBounce = function (width, height) {
-		for (var i = 0; i<this.bodies.length; ++i) {
-			var n = this.bodies[i].checkHitWall(width, height); 
-			if (n != undefined) 
-				this.bodies[i].bounce(n);
-		}
-	}
+    this.draw = function(context) {
+        for (var i = this.bodies.length-1; i>=0; --i) {
+            this.bodies[i].draw(context);
+        }
+    }
 
-	this.checkHit = function () {
-		for (var i = 0; i<this.bodies.length; ++i) {
-			for (var j = i+1; j<this.bodies.length; ++j) {
-				if (this.bodies[i].checkHit(this.bodies[j])) {
-					this.bodies[i].merge(this.bodies[j]);
-					this.bodies.splice(j, 1);
-					--j;
-				}
-			}
-		}
-	}
-	this.nextPosition = function (dt) {
-		var k1 = this.F().multiply(dt);
-		var k2 = this.add(k1.multiply(0.5)).F().multiply(dt);
-		var k3 = this.add(k2.multiply(0.5)).F().multiply(dt);
-		var k4 = this.add(k3).F().multiply(dt); 
-		return this.add(k1.add(k2.multiply(2.0)).add(k3.multiply(2.0)).add(k4).multiply(1.0/6.0));
-	}
+    this.checkBounce = function () {
+        for (var i = 0; i<this.bodies.length; ++i) {
+            if (this.bodies[i].checkHitWall(this.width, this.height))
+                this.bodies[i].bounce(this.width, this.height);
+        }
+    }
 
-	this.draw = function (context) {
-		for (var i = this.bodies.length-1; i>=0; --i) {
-			this.bodies[i].draw(context);
-		}
-	}
+    this.checkHit = function () {
+        for (var i = 0; i<this.bodies.length; ++i) {
+            for (var j = i+1; j<this.bodies.length; ++j) {
+                if (this.bodies[i].checkHit(this.bodies[j])) {
+                    this.bodies[i].merge(this.bodies[j]);
+                    this.bodies.splice(j, 1);
+                    --j;
+                }
+            }
+        }
+    }
+    this.nextPosition = function (dt) {
+        var k1 = this.F().multiply(dt);
+        var k2 = this.add(k1.multiply(0.5)).F().multiply(dt);
+        var k3 = this.add(k2.multiply(0.5)).F().multiply(dt);
+        var k4 = this.add(k3).F().multiply(dt);
+        return this.add(k1.add(k2.multiply(2.0)).add(k3.multiply(2.0)).add(k4).multiply(1.0/6.0));
+    }
+
+    this.draw = function (context) {
+        for (var i = this.bodies.length-1; i>=0; --i) {
+            this.bodies[i].draw(context);
+        }
+    }
 }
 
 function Body (id, pos, v, m) {
-	this.id = id;
-	this.pos = pos;
-	this.v = v;
-	this.m = m;
-	this.radius = Math.pow(m, 1/3.0);
+    this.id = id;
+    this.pos = pos;
+    this.v = v;
+    this.m = m;
+    this.radius = Math.pow(m, 1/3.0);
 
-	this.draw = function (context) {
-		context.fillStyle="black";
+    this.draw = function (context) {
+        context.fillStyle="black";
 
         context.beginPath();
         context.arc(this.pos.x, this.pos.y, this.radius, 0, 2*Math.PI);
         context.fill();
-        context.restore();
-	}
+    }
 
-	this.F = function (that, G) {
-		var result = new Body(id, this.v, new Vector(0, 0), m);
-		var diff = this.pos.subtract(that.pos);
-		result.v = result.v.add(diff.multiply(-G*that.m/cube(diff.norm())));
-		return result;
+    this.F = function (that, G) {
+        var result = new Body(id, this.v, new Vector(0, 0), m);
+        var diff = this.pos.subtract(that.pos);
+        result.v = result.v.add(diff.multiply(-G*that.m/cube(diff.norm())));
+        return result;
 
-		function cube(a) {
-			return a*a*a;
-		}
-	}
+        function cube(a) {
+            return a*a*a;
+        }
+    }
 
-	this.add = function(sp, sv) {
-		return new Body(id, this.pos.add(sp), this.v.add(sv), this.m);
-	}
+    this.add = function(p, v) {
+        return new Body(id, this.pos.add(p), this.v.add(v), this.m);
+    }
 
-	this.checkHit = function (that) {
-		return this.pos.subtract(that.pos).norm() < (this.radius + that.radius);
-	}
+    this.checkHit = function (that) {
+        return this.pos.subtract(that.pos).norm() < (this.radius + that.radius);
+    }
 
-	this.merge = function (that) {
-		this.m += that.m;
-		this.radius = Math.pow(this.m, 1.0/3.0);
-		this.v = this.v.multiply(this.m).add(that.v.multiply(that.m)).multiply(1.0/(this.m + that.m));
-	}
+    this.merge = function (that) {
+        this.m += that.m;
+        this.radius = Math.pow(this.m, 1.0/3.0);
+        this.v = this.v.multiply(this.m).add(that.v.multiply(that.m)).multiply(1.0/(this.m + that.m));
+    }
 
-	this.checkHitWall = function (w, h) {
-		if (this.pos.x-this.radius<=0)
-			return new Vector(1, 0);
-		else if (this.pos.x+this.radius>=w)
-			return new Vector(-1, 0);
-		else if (this.pos.y-this.radius<=0)
-			return new Vector(0, 1);
-		else if (this.pos.y+this.radius>=h)
-			return new Vector(0, -1);
-		else
-			return undefined;
-	}
+    this.checkHitWall = function (w, h) {
+        return this.getNormal(w, h) != undefined;
+    }
 
-	this.bounce = function (n) {;
-		this.v = this.v.add(n.multiply(-2*this.v.scalar(n)));
-	}	
+    this.getNormal = function (w, h) {
+        if (this.pos.x-this.radius<=0)
+            return new Vector(1, 0);
+        else if (this.pos.x+this.radius>=w)
+            return new Vector(-1, 0);
+        else if (this.pos.y-this.radius<=0)
+            return new Vector(0, 1);
+        else if (this.pos.y+this.radius>=h)
+            return new Vector(0, -1);
+        else
+            return undefined;
+    }
+
+    this.bounce = function (w, h) {
+        var n = this.getNormal(w, h);
+        this.v = this.v.add(n.multiply(-2*this.v.scalar(n)));
+    }
 }
 
 function Vector(x, y) {
-	this.x = x;
-	this.y = y;
+    this.x = x;
+    this.y = y;
 
-	this.add = function (that) {
-		return new Vector(this.x+that.x, this.y+that.y);
-	}
+    this.add = function (that) {
+        return new Vector(this.x+that.x, this.y+that.y);
+    }
 
-	this.subtract = function (that) {
-		return new Vector(this.x-that.x, this.y-that.y);
-	}
+    this.subtract = function (that) {
+        return new Vector(this.x-that.x, this.y-that.y);
+    }
 
-	this.multiply = function (a) {
-		return new Vector(this.x*a, this.y*a);
-	}
+    this.multiply = function (a) {
+        return new Vector(this.x*a, this.y*a);
+    }
 
-	this.scalar = function (that) {
-		return this.x*that.x + this.y*that.y;
-	}
+    this.scalar = function (that) {
+        return this.x*that.x + this.y*that.y;
+    }
 
-	this.norm = function (){
-		return Math.sqrt(this.scalar(this));
-	}
+    this.norm = function (){
+        return Math.sqrt(this.scalar(this));
+    }
 }
